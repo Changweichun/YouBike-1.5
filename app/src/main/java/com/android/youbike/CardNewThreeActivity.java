7214528e0447097c34943c3fe257d0a49f5e94d4
fifi-owo <102815605+fifi-owo@users.noreply.github.com>
2022-06-07 22:21:32 +0800

    介面更新 (一卡通新增)

---
+++ app/src/main/java/com/android/youbike/CardNewThreeActivity.java
@@ -0,0 +1,14 @@
+package com.android.youbike;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CardNewThreeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_new_three);
+    }
+}
